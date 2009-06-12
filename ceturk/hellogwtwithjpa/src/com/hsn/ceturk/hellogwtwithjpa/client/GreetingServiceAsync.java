69db217225db775f92b4be43565ffffae80b9fdd
hturksoy <hturksoy@53955f4c-412c-11de-9687-fb973330207e>
2009-06-12 09:23:08 +0000

    initial commit

---
+++ ceturk/hellogwtwithjpa/src/com/hsn/ceturk/hellogwtwithjpa/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package com.hsn.ceturk.hellogwtwithjpa.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback);
+}
